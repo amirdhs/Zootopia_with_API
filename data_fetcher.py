ef6e6a8bc6fce9ea2aa9bc03f714df187896d203
amirdehestani <amirmo800@gmail.com>
2024-12-24 18:55:45 +0100

    Dotenv changes added

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,17 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-name = input("Enter name of animal: ")
-api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(name)
-response = requests.get(api_url, headers={'X-Api-Key': '/fRLricS5LkkRKwnoYQfJg==xR9rhenwOdxzldmD'})
-if response.status_code == requests.codes.ok:
-    print(response.text)
-else:
-    print("Error:", response.status_code, response.text)
 
-# animals = response.json()
-# for animal in animals :
-#     print(animal["name"],animal["taxonomy"])
-# <h2>The animal "goadohjasgfas" doesn't exist.</h2>
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 
 def fetch_data(animal_name):
   """
