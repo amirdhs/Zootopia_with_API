cb348f7bd437080fd3565480ddb701644d6569b5
amirdehestani <amirmo800@gmail.com>
2024-12-25 13:14:47 +0100

    styles added

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,21 +2,56 @@ from data_fetcher import fetch_data
 
 
 def generate_html(data, animal_name):
-    html_content = "<html><body>\n"
+    html_content = """
+    <html>
+    <head>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f4f4f9;
+                color: #333;
+                margin: 20px;
+                padding: 20px;
+            }
+            h2 {
+                color: #4CAF50;
+                border-bottom: 2px solid #4CAF50;
+                padding-bottom: 5px;
+            }
+            ul {
+                list-style-type: none;
+                padding: 10px;
+                background-color: white;
+                border-radius : 10px;
+            }
+            li {
+                margin-bottom: 10px;
+            }
+            .not-found {
+                color: red;
+                font-weight: bold;
+            }
+        </style>
+    </head>
+    <body>
+    """
     if data:
         if len(data) == 0:
-            html_content += f"<h2>The animal '{animal_name}' doesn't exist.</h2>"
+            html_content += f"<h2 class='not-found'>The animal '{animal_name}' doesn't exist.</h2>"
         else:
             for animal in data:
-                html_content += f"<h2>{animal['name']}</h2>\n"
-                html_content += "<ul>\n"
-                html_content += f"<li>Taxonomy: {animal['taxonomy']}</li>\n"
-                html_content += f"<li>Locations: {', '.join(animal['locations'])}</li>\n"
-                html_content += f"<li>Characteristics: {animal['characteristics']}</li>\n"
-                html_content += "</ul>\n"
+                html_content += f"<h2>{animal['name']}</h2>"
+                html_content += "<ul>"
+                html_content += f"<li><strong>Taxonomy:</strong> {animal['taxonomy']}</li>"
+                html_content += f"<li><strong>Locations:</strong> {', '.join(animal['locations'])}</li>"
+                html_content += f"<li><strong>Characteristics:</strong> {animal['characteristics']}</li>"
+                html_content += "</ul>"
     else:
-        html_content += f"<h2>The animal '{animal_name}' doesn't exist.</h2>\n"
-    html_content += "</body></html>"
+        html_content += f"<h2 class='not-found'>The animal '{animal_name}' doesn't exist.</h2>"
+    html_content += """
+    </body>
+    </html>
+    """
     return html_content
 
 
